a80f0bf13bd1f681f082dbf1e0ef67b71507946d
Charlie L <charliesbot@gmail.com>
2021-01-24 21:23:57 -0800

    import files

--- getAllPosts.ts
+++
@@ -1,6 +0,0 @@
-// @ts-ignore
-import { frontMatter as posts } from "./pages/**/blog/**/*.mdx";
-
-const formatPath = (p: string) => p.replace(/\.mdx$/, "");
-
-export { posts, formatPath };
--- pages/es/index.tsx
+++ pages/es/index.tsx
@@ -1,15 +1,14 @@
+// @ts-ignore
+import { frontMatter as posts } from "../es/blog/**/*.mdx";
 import { PostEntry } from "@components/PostEntry";
 import { Layout } from "@components/Layout";
 import { Box } from "@components/ds/Box";
 import { Headline } from "@components/ds/Headline";
-import { posts } from "getAllPosts";
 
 const IndexPage = () => {
-  const sortedDates = [...posts]
-    .filter(({ language }) => language === "es")
-    .sort((a, b) =>
-      new Date(a.publishedAt) < new Date(b.publishedAt) ? 1 : -1
-    );
+  const sortedDates = [...posts].sort((a, b) =>
+    new Date(a.publishedAt) < new Date(b.publishedAt) ? 1 : -1
+  );
 
   return (
     <Layout pageTitle="Charlie's blog" description="">
